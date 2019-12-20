c63bc6b9959288c3737016d5dc0331090b9cc9a9
lucas moura <lucas.j.m@outlook.com>
2019-12-20 14:19:59 -0300

    Classe Calculadora Organizada, esperando avaliação

--- calculadora.py
+++ calculadora.py
@@ -1,34 +1,94 @@
-import main
-def calcular(valor1, valor2, operador):
-    valor1 = valor1
-    valor2 = valor2
-    operador = operador
+#classe calculadora, com todas as funções pertinentes a mesma
+class calculadora():
+	#função para gerenciar modulo
+	def main():
+		print ("Digite o primeiro valor ")
+		valor1 = calculadora.validarValor()
+		
+		
+		print ("Digite o primeiro valor ")
+		valor2 = calculadora.validarValor()
+		
+		print ("Escolha um dos operador (x,+,-,÷)")
+		operador = calculadora.validarOperador()
+		
+		calculadora.calcular(valor1,valor2,operador)
+		
+		print ("Você gostaria de continuar calculando? \n ""S"" para SIM ""N"" para Não")
+		calculadora.deNovo()
+    
+    #função calcular para realizar os calculos dos valores escolhidos pelo usuario e sua operação
+	def calcular(valor1, valor2, operador):
+	    valor1 = valor1
+	    valor2 = valor2
+	    operador = operador
+	    
+	    if (operador == "+"):
+	    	resultado = valor1 + valor2
+	    	return print (valor1," + ", valor2, "= " ,resultado)
+	    	#return calculadora.deNovo()
+	    elif (operador == "-"):
+	    	resultado = valor1- valor2
+	    	return print (valor1," - ",valor2, "= " ,resultado)
+	    	#return calculadora.deNovo()
+	    elif (operador == "*"):
+	    	resultado = valor1 * valor2
+	    	return print (valor1," * ",valor2, "= " ,resultado)
+	    	#return calculadora.deNovo()
+	    elif (operador == "÷"):
+	  	  resultado = valor1 // valor2
+	  	  return print (valor1," ÷ ",valor2, "= " ,resultado)
+	  	  #return calculadora.deNovo()
+	
+    #função que recebe os valores e os valida
+	def validarValor():
+		try:
+			valor = input("")
+			return float(valor)
+		except ValueError:
+			print ("ops acho só aceitamos numeros, tente novamente")
+			return calculadora.validarValor()		
+	
+    #função que recebe os operadores e os valida
+	def validarOperador():
+		operadores = ["*","-","÷","+"]
+		try:
+			operador = input ("")
+			operadores.index(operador)
+			return operador
+		except ValueError:
+			print ("Ops acho que você digitou errado, tente novamente")
+			return calculadora.validarOperador()
+			
+    #funação de controle para o usuario sair do programa ou continuar calculando
+	def deNovo():
+	   calculoDeNovo = input("")
+	   if calculoDeNovo.upper() == 'S':
+	       return calculadora.main()
+	   elif calculoDeNovo.upper() == 'N':
+	      return print('Tudo bem até a proxima!')
+	   else:
+	   	print("Opa só aceitamos ""N"" ou ""S""")
+	   	return  calculadora.deNovo()
+				
 
-    if (operador == "+"):
-        return valor1 + valor2
-    elif (operador == "-"):
-       return  valor1 - valor2
-    elif (operador == "x"):
-       return valor1 * valor2
-    elif (operador == "÷"):
-        return valor1 // valor2
-    else:
-        return "opa! Preciso de um operador"
+'''
+possibilidade de importar cada metodo separadamente
+print ("digite o primeiro valor ")
+valor1 = calculadora.validarValor()
+		
+print ("Digite o segundo valor  ")
+valor2 = calculadora.validarValor()
 
-def validadorDeValores (valor):
-    valor = valor
-    if (valor != int or valor != float):
-        return "Opa só aceitamos números"
-    else:
-        return valor
+print ("Escolha um dos operador (x,+,-,÷)")
+operador = input("")
+calculadora.validarOperador(operador)
+print (operador)
+		
+calculadora.calcular(valor1,valor2,operador)
+calculadora.deNovo()
+'''
 
-def deNovo():
-   calculoDeNovo = input("Você gostaria de continuar calculando? \n ""S"" para SIM ""N"" para Não ")
 
-   if calculoDeNovo.upper() == 'S':
-       valor1, valor2, operador = main.entradas()
-       calcular(valor1, valor2, operador)
-   elif calculoDeNovo.upper() == 'N':
-       print('Tudo bem até a proxima!')
-   else:
-       deNovo()
+if (__name__ == '__main__'):
+	calculadora.main()
