35b42e631419c5eb86f71c47ae37bd979fb3b925
lucas moura <lucas.j.m@outlook.com>
2020-01-06 23:59:09 -0300

    versão 0.0.3

--- calculadora.py
+++ calculadora.py
@@ -2,14 +2,18 @@
 Este módulo é uma projeto de aprendizado, no qual desenvolvemos uma calculadora basica com
 quatro operações.
 
-versão do projeto: 0.0.2
-
+autor lucas jesus de moura
+lucas.j.m@outlook.com 
+versão: 0.0.2
+ 
 """
-
+'''
 #Biblioteca de parâmetros e funções específicos do sistema
+'''
 import sys
-
-# Função que recebe os valores e os converte para float e retorna para a proxima fase
+ '''
+# Função que recebe os valores e os converte para float
+'''
 def validar_valor(valor):
 	valor = valor
 	try:
@@ -17,66 +21,39 @@ def validar_valor(valor):
 		return valor_convertido
 	except ValueError:
 		return False
-
-#valida o operador atraves de uma lista de funções recendo o operador e os valores para passar 
-#como parametro
-def valida_operador(c,a,b):
+ 
+'''
+funções que recebe os valores e chama a função de calculo conrrespondente ao operador
+'''
+def valida_operador(operador,primeiro_valor_float,segundo_valor_float):
 	operadores = {"+":adicao,"-":subtracao,"*":multiplicacao,"/":divisao}
 	try:
-		operadores[c](a,b)
+		print(operadores[operador](primeiro_valor_float,segundo_valor_float))
 	except KeyError:
 		return False
-
+ 
 '''
 funções para efetuar os calculos juntamente com a função para finalizar ou continuar o programa
 '''
 def adicao(valor1, valor2):
-	resultado = valor1 + valor2
-	print (valor1," + ", valor2," = ",resultado)
-	perguntar()
+	return valor1 + valor2
 	
-
 def subtracao(valor1, valor2):
-	resultado = valor1 - valor2
-	print (valor1," - ",valor2," = ",resultado)
-	perguntar()
-	 
+	return valor1 - valor2
+	
 def multiplicacao(valor1, valor2):
-	resultado = valor1 * valor2
-	print (valor1," * ",valor2," = ",resultado)
-	perguntar()
-	    
+	return valor1 * valor2
+	
 def divisao(valor1, valor2):
 	try:
-		resultado = valor1 / valor2
-		print (valor1," / ",valor2," = ",resultado)
-		perguntar()
+		return valor1 / valor2
 	except ZeroDivisionError:
-		print("ops nao dividimos por zero")
-		perguntar()
-		
-#pergunta para o usuario se ele deseja iniciar o programa e valida sua resposta	
-def calcular():
-	x = True
-	while x == True:
-		calculoDeNovo = input("")
-		if calculoDeNovo.upper() == 'S':
-		   print("Então vamos lá")
-		   x = False
-		   validar_entradas()
-		  # return main()
-		elif calculoDeNovo.upper() == 'N':
-			print('Tudo bem até a proxima!')
-			return sys.exit()
-		else:
-		   print("Opa só aceitamos ""N"" ou ""S""")
-
-#pergunta para o usuario se o mesmo quer finalizar o programa	   
-def perguntar():
-	print("Gostaria de calcular de novo?")
-	calcular()
-
-#função que valida as entradas
+		return "ops nao dividimos por zero"
+	
+'''
+funação que valida os valores de entrada
+em forma de split e chama para os a validadores
+'''
 def validar_entradas():
 	print("Para começarmoa digite os valores a ser calculado,\
     juntamente com o operador desejado como mostra o exemplo abaixo:\n ""45 + 89""")
@@ -84,36 +61,54 @@ def validar_entradas():
 	x = True
 	while x == True:
 		try:
-			a,b,c=input("").split()
+			primeira_entrada,entrada_operador,segunda_entrada=input("").split()
 			x = False
 		except ValueError:
 			print("Ops, se atente ao espaço entre os valores e o operador")
-
+ 
     #chamada das funções que valida os valores e os retorna convertidos
-	a = validar_valor(a)
-	c = validar_valor (c)
-	b = valida_operador(b,a,c)
+	primeira_entrada = validar_valor(primeira_entrada)
+	segunda_entrada = validar_valor (segunda_entrada)
+	entrada_operador = valida_operador(entrada_operador,primeira_entrada,segunda_entrada)
 	
     #laço que valida os valores individualmente caso, haja algum errado
-	while a == False or b == False or c == False:
-		if a == False:
-			a=input("Ops acho que você digitou o primeiro valor errado ")
-			a = validar_valor(a)
-		elif c == False:
-			c = input("Ops acho que você digitou o segundo valor errado ")
-			c = validar_valor(c)
-		elif b == False:
-			b = input("Ops acho que você digitou o operador valor errado ")
-			b = valida_operador(b,a,c)
-
-#função main que faz a apresentação e chama a primeira função
+	while primeira_entrada == False or entrada_operador == False or segunda_entrada == False:
+		if primeira_entrada == False:
+			primeira_entrada=input("Ops acho que você digitou o primeiro valor errado ")
+			primeira_entrada = validar_valor(primeira_entrada)
+		elif segunda_entrada == False:
+			segunda_entrada = input("Ops acho que você digitou o segundo valor errado ")
+			segunda_entrada = validar_valor(segunda_entrada)
+		elif entrada_operador == False:
+			entrada_operador = input("Ops acho que você digitou o operador valor errado ")
+			entrada_operador = valida_operador(entrada_operador,primeira_entrada,segunda_entrada)
+ 
+'''
+Função que valida que inicia o programa em forma de loop até que o usario deseje sair
+'''
 def main():
     print("Olá, bem vindo a calcular, se trata de um\n projeto de aprendizado\
-        onde desenvolvemos uma calculadora que realiza quatro operações básicas:\
-        Multiplicação, Divisão, Adição e\ subtração.\nVamos nessa? \n(""S"" para SIM\
-        ""N"" para Não)")
+		onde desenvolvemos uma calculadora que realiza quatro operações básicas:\
+        Multiplicação, Divisão, Adição e subtração.Vamos nessa? \n(""S"" para SIM\
+		""N"" para Não)")
     #Função que os primeiros valores
-    calcular()
-				
+    prime_vez = 0
+    while True:
+    	if prime_vez == 1:
+    		print("quer calcular de novo?")
+    	prime_vez = 1
+    	x = True
+    	while x == True:
+    		calculo_de_novo= input("")
+    		if calculo_de_novo.upper() == 'S':
+    			print("Então vamos lá")
+    			x = False
+    			validar_entradas()	
+    		elif calculo_de_novo.upper() == 'N':
+    			print('Tudo bem até a proxima!')
+    			return sys.exit()
+    		else:
+    			print("Opa só aceitamos ""N"" ou ""S""")
+    	    				
 if __name__ == '__main__':
     main()
